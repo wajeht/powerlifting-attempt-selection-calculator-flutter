c149030acdbe2ae24ef2c60b9bdc09a5d9401e29
wajeht <58354193+wajeht@users.noreply.github.com>
2022-12-11 21:47:42 -0500

    feat: added logo on welcome page

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -13,53 +13,95 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Attempt Selection Calculator')),
-      body: Padding(
+      body: Container(
         padding: const EdgeInsets.all(10),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Container(),
-            Container(
-              child: Column(children: [
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () => context.push('/login'),
-                    child: const Text('Login with Email'),
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.2,
                   ),
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () => context.push('/register'),
-                    child: const Text('Register an Account'),
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: SizedBox.fromSize(
+                        size: const Size.fromRadius(100),
+                        child: Image.asset(
+                          'assets/images/louie.jpg',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () => context.go('/attempts'),
-                    child: const Text('Continue as Guest'),
-                  ),
-                )
-              ]),
+                  // const SizedBox(
+                  //   height: 30,
+                  // ),
+                  // const Text('Attempt Selection Calculator'),
+                ],
+              ),
             ),
-            SizedBox(
-              width: double.infinity,
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.black,
-                ),
-                onPressed: () async {
-                  final Uri url = Uri.parse('https://www.github.com/wajeht');
-                  if (!await launchUrl(url)) {
-                    throw 'Could not launch $url';
-                  }
-                },
-                child: const Text(
-                  'Made with ❤️ by @wajeht ↗',
-                ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: <Widget>[
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () => context.push('/login'),
+                          child: const Text('Login or Register'),
+                        ),
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                            side: const BorderSide(width: 1, color: Colors.red),
+                          ),
+                          child: const Text('Continue as Guest'),
+                          onPressed: () => context.push('/attempts'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        shadowColor: Colors.red,
+                      ),
+                      onPressed: () async {
+                        final Uri url =
+                            Uri.parse('https://www.github.com/wajeht');
+                        if (!await launchUrl(url)) {
+                          throw 'Could not launch $url';
+                        }
+                      },
+                      child: const Text(
+                        'Made with ❤️ by @wajeht ↗',
+                        style: TextStyle(shadows: <Shadow>[
+                          Shadow(
+                            offset: Offset(0, 0),
+                            blurRadius: 20,
+                            color: Colors.white,
+                          ),
+                        ]),
+                      ),
+                    ),
+                  )
+                ],
               ),
             )
           ],
