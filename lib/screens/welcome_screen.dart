ecf0abec14ee3e5eb2dbf454fb78a122a92baf61
wajeht <58354193+wajeht@users.noreply.github.com>
2022-12-11 20:21:29 -0500

    feat: added made by footer link

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -12,30 +13,53 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('Attempt Selection Calculator')),
       body: Padding(
-        padding: const EdgeInsets.only(left: 10, right: 10),
+        padding: const EdgeInsets.all(10),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () => context.push('/login'),
-                child: const Text('Login with Email'),
-              ),
+            Container(),
+            Container(
+              child: Column(children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () => context.push('/login'),
+                    child: const Text('Login with Email'),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () => context.push('/register'),
+                    child: const Text('Register an Account'),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () => context.go('/attempts'),
+                    child: const Text('Continue as Guest'),
+                  ),
+                )
+              ]),
             ),
             SizedBox(
               width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () => context.push('/register'),
-                child: const Text('Register an Account'),
-              ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () => context.go('/attempts'),
-                child: const Text('Continue as Guest'),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.black,
+                ),
+                onPressed: () async {
+                  final Uri url = Uri.parse('https://www.github.com/wajeht');
+                  if (!await launchUrl(url)) {
+                    throw 'Could not launch $url';
+                  }
+                },
+                child: const Text(
+                  'Made with ❤️ by @wajeht ↗',
+                ),
               ),
             )
           ],
